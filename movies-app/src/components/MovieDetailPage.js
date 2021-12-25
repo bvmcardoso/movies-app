ce59d9b47a33c9371558716e42b4296e1327d961
Bruno Vinícius <bvmcardoso@gmail.com>
2021-12-25 16:20:05 -0300

    refactor: polish movie detail page

--- movies-app/src/components/MovieDetailPage.js
+++ movies-app/src/components/MovieDetailPage.js
@@ -2,36 +2,61 @@
 
 import { useEffect, useState } from "react";
 function MovieDetailPage(props) {
-	useEffect(() => {
-		const imdbId = props.match.params.imdbId;
-		fetchMovieDetailsById(imdbId);
-	}, []);
+    useEffect(() => {
+        const imdbId = props.match.params.imdbId;
+        fetchMovieDetailsById(imdbId);
+    }, []);
 
-	const [movieDetail, setMovieDetail] = useState({});
+    const [movieDetail, setMovieDetail] = useState({});
 
-	const fetchMovieDetailsById = (movieId) => {
-		const movieDetailsUrl = `http://www.omdbapi.com/?i=${movieId}&apikey=d0cda79`;
+    const fetchMovieDetailsById = (movieId) => {
+        const movieDetailsUrl = `http://www.omdbapi.com/?i=${movieId}&apikey=d0cda79`;
 
-		fetch(movieDetailsUrl)
-			.then((response) => response.json())
-			.then((result) => {
-				console.log(result);
-				setMovieDetail(result);
-			});
-	};
+        fetch(movieDetailsUrl)
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result);
+                setMovieDetail(result);
+            });
+    };
 
-	return (
-		<div>
-			<img src={movieDetail.Poster} />
-			<h5>{movieDetail.Title}</h5>
-			<p>{movieDetail.Plot}</p>
-			<p>Released:{movieDetail.Released}</p>
-			<p>Director: {movieDetail.Director}</p>
-			<p>Writer: {movieDetail.Writer}</p>
-			<p>Actors: {movieDetail.Actors}</p>
-			<p>Awards: {movieDetail.Awards}</p>
-		</div>
-	);
+    return (
+        <div class="container">
+            <div className="row justify-content-center">
+                <div className="col col-lg-6 mt-5">
+                    <div className="text-center">
+                        <img src={movieDetail.Poster} />
+                        <h1 class="mt-3">{movieDetail.Title}</h1>
+                    </div>
+                    <table class="table table-hover mt-3">
+                        <tbody>
+                            <tr>
+                                <th scope="row">Plot</th>
+                                <td>{movieDetail.Plot}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Director</th>
+                                <td>{movieDetail.Director}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Writer</th>
+                                <td>{movieDetail.Writer}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Actors</th>
+                                <td>{movieDetail.Actors}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Awards</th>
+                                <td>{movieDetail.Awards}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default MovieDetailPage;
