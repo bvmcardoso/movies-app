7041361a7c56c60c03303b5d28b9a078bf685b2e
Bruno Vinícius <bvmcardoso@gmail.com>
2021-12-25 12:30:55 -0300

    feat: polish movie list page

--- movies-app/src/components/MovieListPage.js
+++ movies-app/src/components/MovieListPage.js
@@ -1,82 +1,104 @@
-import { useState, useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
+/** @format */
 
-
+import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 function MovieListPage() {
-    
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState("");
 
-    const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([]);
 
-    const [noMoviesFound, setNoMoviesFound] = useState(false)
+    const [noMoviesFound, setNoMoviesFound] = useState(false);
 
     const handleSearchTermChange = (e) => {
         setSearchTerm(e.target.value);
-    }
+    };
 
     const clearResults = () => {
-        localStorage.removeItem("searchTerm")
-        setMovies([])
-        setSearchTerm('')
-        
-    }
+        localStorage.removeItem("searchTerm");
+        setMovies([]);
+        setSearchTerm("");
+    };
 
     useEffect(() => {
         // get movie from local storage
-        let term = localStorage.getItem("searchTerm")
+        let term = localStorage.getItem("searchTerm");
         if (term) {
-            fetchMovies(term)
+            fetchMovies(term);
         }
-    }, [])
+    }, []);
 
     const fetchMovies = (movieName) => {
-        const searchUrl = `http://www.omdbapi.com/?s=${movieName}&apikey=d0cda79`
+        const searchUrl = `http://www.omdbapi.com/?s=${movieName}&apikey=d0cda79`;
 
         // put movie name in local storage
-        localStorage.setItem("searchTerm", movieName)
+        localStorage.setItem("searchTerm", movieName);
 
         fetch(searchUrl)
-        .then(response => response.json())
-            .then(result => {
-
+            .then((response) => response.json())
+            .then((result) => {
                 if (result.Error) {
-                    setMovies([])
-                    setNoMoviesFound(true)
+                    setMovies([]);
+                    setNoMoviesFound(true);
+                } else {
+                    console.log(result)
+                    setMovies(result.Search);
+                    setNoMoviesFound(false);
                 }
-                else {
-                    setMovies(result.Search)
-                    setNoMoviesFound(false)
-                }
-                
-        })
-    }
+            });
+    };
 
-
-    const movieItems = movies.map(movie => {
+    const movieItems = movies.map((movie) => {
         return (
-            <div key={movie.imdbID}>
-                <h3>{movie.Title}</h3>
-                <img src={movie.Poster} />
-                <br/>
-                <NavLink to={`/${movie.imdbID}`}>
-                    <button>Details</button>
-                </NavLink>
+            <div class="col" key={movie.imdbID}>
+                <div class="card shadow-sm">
+                    <img src={movie.Poster === "N/A" ? "/missing_movie.jpg" : movie.Poster} class="bd-placeholder-iomg card-img-top" alt={movie.Title}/>
+                    <div class="card-body">
+                        <h4 class="card-text">{movie.Title}</h4>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <NavLink to={`/${movie.imdbID}`}>
+                                    <button class="btn btn-sm btn-outline-secondary"> Details </button>{" "}
+                                </NavLink>{" "}
+                            </div>
+                            <small class="text-muted">Year: {movie.Year}</small>
+                        </div>
+                    </div>
+                </div>
             </div>
-        )
-    })
+        );
+    });
 
     return (
         <div>
-            <h1>Movie list page</h1>
-            Search: <input type="text" onChange={ handleSearchTermChange } />
-            <button onClick={() => fetchMovies(searchTerm)}>Search</button>
-            <button onClick={clearResults}>Clear results</button>
-            {movieItems}
+            <section class="py-5 text-center container">
+                <div class="row py-lg-5">
+                    <div class="col-lg-6 col-md-8 mx-auto">
+                        <h1 class="fw-light">Movie Search</h1>
+                        <p class="lead text-muted">We want to help you find the right movie. Start a search and see what you'll be watching this night!</p>
+                        <p>
+                            <input type="text" onChange={handleSearchTermChange} placeholder="ex. Batman" />
+                        </p>
+                        <p>
+                            <button class="btn btn-primary my-2 me-2" onClick={() => fetchMovies(searchTerm)}> Search </button>
+                            <button class="btn btn-secondary my-2" onClick={clearResults}> Clear results </button>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <div class="album py-5 bg-light">
+                <div class="container">
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                        {movieItems}
+                    </div>
+                </div>
+            </div>
 
-            {noMoviesFound ? <h1>No movies found</h1> : null}
+
+            {noMoviesFound ? <h1> No movies found </h1> : null}{" "}
+
         </div>
-    )
+    );
 }
 
-export default MovieListPage
+export default MovieListPage;
